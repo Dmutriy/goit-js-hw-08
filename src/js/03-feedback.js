644ae397e0b0e6067928551030d7670b7b38041f
Dmutriy <105307970+Dmutriy@users.noreply.github.com>
2022-10-02 20:42:41 +0300

    Fedbacck

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,92 @@
+// 'use strict';
+import throttle from 'lodash.throttle';
+const LOCALE_STORAGE_KEY = 'contact-form-key';
+import { load, save, remove } from './storage';
+const formRef = document.querySelector('.feedback-form');
 
+initPage();
+
+const onFormInput = event => {
+  //   console.log(event.target.name);
+  //   console.log(event.target.value);
+  const { name, value } = event.target;
+
+  let saveData = load(LOCALE_STORAGE_KEY);
+  saveData = saveData ? saveData : {};
+
+  saveData[name] = value;
+
+  save(LOCALE_STORAGE_KEY, saveData);
+};
+
+// done
+const throttledOnFormInput = throttle(onFormInput, 500);
+formRef.addEventListener('input', throttledOnFormInput);
+
+function initPage() {
+  const saveData = load(LOCALE_STORAGE_KEY);
+
+  if (!saveData) {
+    return;
+  }
+  //   console.log(Object.entries(saveData));
+  Object.entries(saveData).forEach(([name, value]) => {
+    formRef.elements[name].value = value;
+  });
+}
+
+const handleSubmit = event => {
+  event.preventDefault();
+
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+
+  console.log({ email: email.value, message: message.value });
+  event.currentTarget.reset();
+  remove(LOCALE_STORAGE_KEY);
+};
+
+formRef.addEventListener('submit', handleSubmit);
+
+////////////////////////////////////////////////
+
+// import throttle from 'lodash.throttle';
+// import { load, save, remove } from './storage';
+// // +++++++++++++++++++++++++++++++++++++++++++
+// const LOCALSTORAGE_FORM_KEY = 'feedback-form-state';
+// const formRef = document.querySelector('.feedback-form');
+// // +++++++++++++++++++++++++++++++++++++++++++
+// initPage();
+
+// const onFormInput = event => {
+//   const { name, value } = event.target;
+//   let saveData = load(LOCALSTORAGE_FORM_KEY);
+//   saveData = saveData ? saveData : {};
+//   saveData[name] = value;
+//   save(LOCALSTORAGE_FORM_KEY, saveData);
+// };
+
+// const throttleOnFormInput = throttle(onFormInput, 500);
+// formRef.addEventListener('input', throttleOnFormInput);
+// // -------------------------------------------
+// function initPage() {
+//   const saveData = load(LOCALSTORAGE_FORM_KEY);
+//   if (!saveData) {
+//     return;
+//   }
+//   Object.entries(saveData).forEach(([name, value]) => {
+//     formRef.elements[name].value = value;
+//   });
+// }
+// // -------------------------------------------
+// const onSubmit = event => {
+//   event.preventDefault();
+//   const {
+//     elements: { email, message },
+//   } = event.currentTarget;
+//   event.currentTarget.reset();
+//   remove(LOCALSTORAGE_FORM_KEY);
+// };
+
+// formRef.addEventListener('submit', onSubmit);
